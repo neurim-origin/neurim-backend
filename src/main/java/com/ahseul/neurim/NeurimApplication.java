9cfc47a52569c20838969a9de0670fe2966538e9
seulhee <lsh9412@yanadoocorp.com>
2022-04-03 15:43:49 +0900

    first commit

---
+++ src/main/java/com/ahseul/neurim/NeurimApplication.java
@@ -0,0 +1,13 @@
+package com.ahseul.neurim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeurimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeurimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahseul/neurim/NeurimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahseul.neurim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeurimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
